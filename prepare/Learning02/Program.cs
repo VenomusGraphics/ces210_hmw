aa028694b38d59b718f535635e9265dde3b4c519
calvin hewett <hewecalv@byui.edu>
2023-09-21 09:47:01 -0600

    new files to add updating soon

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Formats.Asn1;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
+    // not going to lie i definly didnt unerstand how to get to the get my jobnum1 workin so i looked at the sample
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello user!");
+
+        JobsTitle JobNum1 = new JobsTitle();
+        
+        JobNum1._JobTitle = "engeriner";
+        JobNum1._companey ="Microsoft"
+        JobNum1._startyear= 2019;
+        JobNum1._endyear  =2023;
+
+        
+
+        
+    
+
+
+
+
     }
+
+    
+
+
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,7 @@
+using System.Reflection.Metadata;
+
+public class Resume
+{
+    public string _name;
+    public list<Job>
+}
