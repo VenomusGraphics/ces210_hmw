d3b82018f477008be6c380051c1e86e352b74703
calvin hewett <hewecalv@byui.edu>
2023-09-21 18:03:25 -0600

    code done'

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,15 +12,25 @@ class Program
         JobsTitle JobNum1 = new JobsTitle();
         
         JobNum1._JobTitle = "engeriner";
-        JobNum1._companey ="Microsoft"
+        JobNum1._companey ="Microsoft";
         JobNum1._startyear= 2019;
         JobNum1._endyear  =2023;
 
         
+        JobsTitle JobNum2 = new JobsTitle();
 
-        
-    
+        JobNum2._JobTitle = "Developer";
+        JobNum2._companey ="Google";
+        JobNum2._startyear= 2022;
+        JobNum2._endyear  =2023;
 
+        Resume Resumes = new Resume();
+        Resumes. _name ="David Row";
+
+        Resumes._Job.Add(JobNum1);
+        Resumes._Job.Add(JobNum2);
+
+        Resumes.Display();
 
 
 
