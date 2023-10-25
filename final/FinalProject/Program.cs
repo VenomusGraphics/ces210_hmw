56e433fa58077abd5f8ffe46e9adce904717cd8d
calvin hewett <hewecalv@byui.edu>
2023-10-24 23:15:29 -0600

    well this is what we could get done. dont work compleatly but it will do

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Console.WriteLine("Welcome to the Text Adventure Game!");
+
+        Game textAdventureGame = LoadOrCreateGame(); // Load or create a new game
+
+        textAdventureGame.Start();
+
+       
+    static Game LoadOrCreateGame()
+    {
+        Console.WriteLine("Load a saved game? (yes/no): ");
+        string input = Console.ReadLine().ToLower();
+
+       
+        return new Game(); // Create a new game if not loading a saved game
     }
-}
+ }
+ }
